fd65aee5a9856afedc2a2370dd5adf8e18f5c41f
Denis <144208071+D3ONISIO@users.noreply.github.com>
2024-05-23 11:01:44 +0200

    1) Added new buttons for calculator 1-9 and some actions (additions, substractions and equals) 2) Added new method, which changes a value of our text 3) Some cosmetics changes

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -22,26 +22,78 @@ public class MainActivity extends AppCompatActivity {
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button additionButton = findViewById(R.id.additionButton);
+        Button substractionButton = findViewById(R.id.substractionButton);
+        Button equalButton = findViewById(R.id.equalButton);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("Hello World!");
+                textView.setText("1");
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("Denis Olshanskyi");
+                textView.setText("2");
             }
         });
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                textView.setText("Chao!");
-            }
+            public void onClick(View v) {textView.setText("3");}
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("4");}
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("5");}
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("6");}
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("7");}
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("8");}
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("9");}
+        });
+
+        additionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("+");}
+        });
+
+        substractionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("-");}
+        });
+
+        equalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {textView.setText("=");}
         });
     }
 }
